4c9690eb3ba7c0b40d56588fb8bfe135a6c39ef0
Niraj Pant <nirajpant7@gmail.com>
2014-12-05 10:57:45 -0600

    Initial Commit

---
+++ BPA-Regionals/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BPA-Regionals
+//
+//  Created by Niraj  on 12/5/14.
+//  Copyright (c) 2014 Niraj Pant. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
