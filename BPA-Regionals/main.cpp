59d0c5d336167b12d9c5508dabd79420895d7741
Niraj Pant <nirajpant7@gmail.com>
2014-12-05 11:28:40 -0600

    init

--- BPA-Regionals/main.cpp
+++ BPA-Regionals/main.cpp
@@ -7,9 +7,34 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+int calcLeibniz(int n) {
+    int piApprox = 0;
+    for (int i=0; i < n; ++i) {
+//        std::cout << "i: " << i << "\n";
+        int power = pow (-1, n);
+        if(i < 5)
+        {
+            printf("power is: %d", power);
+        }
+//        piApprox = piApprox + (power/(2*n +1));
+    }
+    
+    return piApprox;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    int PI = 3.141592653589793;
+    float approx1 = (22/7);
+    float approx2 = (355/113);
+    int LEIBNIZ_const = 1000000;
+    calcLeibniz(LEIBNIZ_const);
+    
     std::cout << "Hello, World!\n";
+    std::cout << calcLeibniz(LEIBNIZ_const);
     return 0;
 }
+
+
